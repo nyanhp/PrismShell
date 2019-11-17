a20ea9e9c05cb98bf25f9a33c3cf1a65e9f1ff13
nyanhp <nyanhp@users.noreply.github.com>
2019-11-17 18:27:57 +0100

    Pause and Resume implemented

---
+++ functions/Resume-PrismPrint.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+    Resume a print on your Prism!
+.DESCRIPTION
+    Resume a print on your Prism!
+.PARAMETER ComputerName
+    The host name or IP of your Prism
+.PARAMETER Session
+    The session to your Prism, autocreated if not provided
+#>
+function Resume-PrismPrint
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]
+        $ComputerName,
+
+        [Parameter()]
+        [microsoft.powershell.commands.webrequestsession]
+        $Session
+    )
+
+    $uri = "http://$ComputerName/CMD/Resume"
+    if ($null -eq $Session)
+    {
+        $Session = New-PrismSession -ComputerName $ComputerName
+    }
+
+    if ((Get-PrismStatus -ComputerName $ComputerName -Session $Session).Status -in 'Idle','Unknown')
+    {
+        Write-Warning -Message 'Not executing Resume. Printer is currently idle'
+    }
+
+    Invoke-RestMethod -Uri $uri -Method Get -WebSession $Session
+}
---
+++ functions/Suspend-PrismPrint.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+    Pause a print on your Prism!
+.DESCRIPTION
+    Pause a print on your Prism!
+.PARAMETER ComputerName
+    The host name or IP of your Prism
+.PARAMETER Session
+    The session to your Prism, autocreated if not provided
+#>
+function Suspend-PrismPrint
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]
+        $ComputerName,
+
+        [Parameter()]
+        [microsoft.powershell.commands.webrequestsession]
+        $Session
+    )
+
+    $uri = "http://$ComputerName/CMD/Pause"
+    if ($null -eq $Session)
+    {
+        $Session = New-PrismSession -ComputerName $ComputerName
+    }
+
+    if ((Get-PrismStatus -ComputerName $ComputerName -Session $Session).Status -in 'Idle','Unknown')
+    {
+        Write-Warning -Message 'Not executing pause. Printer is currently idle'
+    }
+
+    Invoke-RestMethod -Uri $uri -Method Get -WebSession $Session
+}
