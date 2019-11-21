921a7b5a29009e22db9fc0fbd339dd63a9ef54dd
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-11-21 10:04:59 +0100

    whoops

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -7,10 +7,3 @@ foreach ($module in $modules) {
     Install-Module $module -Force -SkipPublisherCheck -AllowClobber
     Import-Module $module -Force -PassThru
 }
-
-$binaryPath = Join-Path -Path (Get-Module PSFramework).ModuleBase -ChildPath Bin\PSFramework.dll -Resolve -ErrorAction Stop
-$projPath = Join-Path -Path $PSScriptRoot -ChildPath '..\library\PrismShell\PrismShell\PrismShell.csproj'  -Resolve -ErrorAction Stop
-
-$content = [xml](Get-Content -Path $projPath)
-($content.Project.ItemGroup.Reference | Where-Object Include -eq PSFramework).HintPath = $binaryPath -replace '\\','/'
-$content.Save($projPath)
